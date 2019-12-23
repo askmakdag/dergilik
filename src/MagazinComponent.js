a8184503d48b0e522e0269e223272d12ee919a57
askmakdag <aakdag@etu.edu.tr>
2019-12-23 19:30:05 +0300

    android side bug fixed about lib. linking.

--- src/MagazinComponent.js
+++ src/MagazinComponent.js
@@ -15,9 +15,10 @@ export default class MagazinComponent extends Component {
 
     render() {
         const source = {uri: '/Users/macbook/Desktop/dergilik/assets/The_Economist_USA_-_30_11_2019.pdf'}; //require('../assets/The_Economist_USA_-_30_11_2019.pdf');
+        const source2 = require('../assets/The_Economist_USA_-_30_11_2019.pdf');
 
         return <Pdf
-            source={source}
+            source={source2}
             style={styles.pdf}
         />;
     }
@@ -25,7 +26,6 @@ export default class MagazinComponent extends Component {
 
 const styles = StyleSheet.create({
     pdf: {
-        flex: 1,
         height: Dimensions.get('window').height,
         width: Dimensions.get('window').width,
         justifyContent: 'center',
