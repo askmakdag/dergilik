00962d02bdb034398d4c449840cc17dae760ba9f
askmakdag <aakdag@etu.edu.tr>
2019-12-27 20:55:38 +0300

    Start to redesign authentication pages.

--- Authentication/SignIn.js
+++ Authentication/SignIn.js
@@ -6,12 +6,15 @@ import {
     Text,
     Alert,
     Dimensions,
+    Image, TouchableOpacity,
 } from 'react-native';
-import {Button} from 'react-native-elements';
+import {Button, CheckBox,Icon} from 'react-native-elements';
 import Amplify, {Auth} from 'aws-amplify';
 import aws_exports from '../aws-exports.js';
 
 Amplify.configure(aws_exports);
+const backgroundColor = '#FBFCFC';
+import countries from '../assets/countries';
 
 class SignIn extends Component {
 
@@ -20,6 +23,8 @@ class SignIn extends Component {
         this.state = {
             cell_phone: '',
             confirmation_code: '',
+            checked: false,
+            country: {name: 'turkey', code: '+90', flag: require('../assets/counrty_flags/turkey.png')},
         };
     };
 
@@ -48,18 +53,49 @@ class SignIn extends Component {
 
                 <View>
                     <Text style={styles.gsmTitleTextStyle}>Gsm Numaranız</Text>
-                    <TextInput
-                        onChangeText={(value) => this.onChangeText('cell_phone', value)}
-                        style={styles.input}
-                        placeholder="Telefon numaranızı giriniz"
-                        placeholderTextColor={'#434241'}
-                        keyboardType={'numeric'}
+
+                    <View style={{
+                        flexDirection: 'row',
+                        justifyContent: 'flex-start',
+                        alignItems: 'center',
+                        backgroundColor: '#DCDDDE',
+                        marginVertical: 5,
+                    }}>
+                        <TouchableOpacity style={{flexDirection: 'row', justifyContent: 'flex-start', alignItems: 'center'}}>
+                            <Image source={this.state.country.flag}
+                                   style={{height: 26, width: 26, marginHorizontal: 5}}/>
+                            <Text style={{fontSize: 13, marginRight: 5}}>{this.state.country.code}</Text>
+                            <Icon
+                                name='sort-down'
+                                type='font-awesome'
+                                size={13}
+                                containerStyle={{marginBottom:5,marginRight:5}}
+                                color='#2A2A2A' />
+                        </TouchableOpacity>
+
+                        <TextInput
+                            onChangeText={(value) => this.onChangeText('cell_phone', value)}
+                            style={styles.input}
+                            placeholder="Telefon numaranızı giriniz"
+                            placeholderTextColor={'#434241'}
+                            keyboardType={'numeric'}
+                        />
+                    </View>
+                </View>
+
+                <View style={styles.checkBoxOuterContainer}>
+                    <CheckBox
+                        center
+                        title='Numaramı Hatırla'
+                        checked={this.state.checked}
+                        containerStyle={styles.checkBoxContainerStyle}
+                        onPress={() => this.setState({checked: !this.state.checked})}
                     />
                 </View>
                 <View style={styles.buttonsContainerStyle}>
-                    <Button title={'Giriş Yap'} onPress={() => this.SignIn()}/>
-
-                    <Button title={'Kayıt Ol'} onPress={() => this.props.navigation.navigate('SignUp')}/>
+                    <Button title={'Giriş Yap'} onPress={() => this.SignIn()} buttonStyle={styles.signInButtonStyle}/>
+                    <Button title={'Kayıt Ol'} onPress={() => this.props.navigation.navigate('SignUp')}
+                            buttonStyle={styles.signUpButtonStyle} titleStyle={styles.signUpButtonTittleStyle}/>
                 </View>
             </View>
         );
@@ -70,34 +106,67 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'flex-start',
         alignItems: 'center',
+        backgroundColor: backgroundColor,
+        marginVertical: '20%',
     },
     input: {
-        textAlign: 'center',
-        height: 45,
+        textAlign: 'left',
+        padding: 5,
+        height: 50,
+        fontSize: 15,
+        fontWeight: '500',
         color: '#000',
         backgroundColor: '#F2F3F4',
-        width: Dimensions.get('window').width * 0.96,
-        //borderBottomWidth: 0.5,
+        width: Dimensions.get('window').width * 0.75,
         borderWidth: 1.5,
         borderColor: '#D7DBDD',
-        //borderBottomColor: '#474646',
     },
     buttonsContainerStyle: {
-        width: Dimensions.get('window').width * 0.6,
         flexDirection: 'column',
         alignItems: 'center',
         justifyContent: 'flex-start',
+        marginVertical: 15,
     },
     gsmTitleTextStyle: {
         fontSize: 16,
-        fontWeight: '600',
+        fontWeight: '500',
         color: '#1a1a1a',
         marginVertical: 10,
+        marginLeft: 4,
     },
     gsmContainer: {
         flexDirection: 'column',
+    },
+    signInButtonStyle: {
+        width: Dimensions.get('window').width * 0.92,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: '#0497EC',
+    },
+    signUpButtonStyle: {
+        backgroundColor: backgroundColor,
+        marginTop: 10,
+    },
+    signUpButtonTittleStyle: {
+        color: '#0497EC',
+        fontSize: 16,
+        fontWeight: '400',
+    },
+    checkBoxOuterContainer: {
+        flexDirection: 'row',
+        justifyContent: 'flex-start',
+        width: Dimensions.get('window').width,
+        marginVertical: 15,
+    },
+    checkBoxContainerStyle: {
+        backgroundColor: backgroundColor,
+        borderColor: backgroundColor,
+    },
+    imageStyle: {
+        width: Dimensions.get('window').width * 0.7,
+        height: Dimensions.get('window').width * 0.4,
     },
 });
 
---
+++ assets/countries.js
@@ -0,0 +1,8 @@
+export const countries = [
+    {name: 'usa', code: '+01', flag: require('../assets/counrty_flags/usa.png')},
+    {name: 'turkey', code: '+90', flag: require('../assets/counrty_flags/turkey.png')},
+    {name: 'belgium', code: '+90', flag: require('../assets/counrty_flags/belgium.png')},
+    {name: 'china', code: '+90', flag: require('../assets/counrty_flags/china.png')},
+    {name: 'russia', code: '+90', flag: require('../assets/counrty_flags/russia.png')},
+    {name: 'germany', code: '+90', flag: require('../assets/counrty_flags/germany.png')},
+];
