90a6dd558f654d758dfa2b6a1e92d8ff2858473a
askmakdag <aakdag@etu.edu.tr>
2019-12-29 17:32:02 +0300

    SignUp page design changed.

--- Authentication/SignUp.js
+++ Authentication/SignUp.js
@@ -6,10 +6,11 @@ import {
     TextInput,
     View,
     Alert,
-    Dimensions, Button,
+    Dimensions,
 } from 'react-native';
 import Amplify, {Auth} from 'aws-amplify';
 import aws_exports from '../aws-exports.js';
+import {Button} from 'react-native-elements';
 
 Amplify.configure(aws_exports);
 
@@ -49,28 +50,40 @@ class SignUp extends Component {
         }
     };
 
-    render() {
-        const {display_button} = this.state;
+    navigateTo = () => {
 
+    };
+
+    render() {
         return (
             <View style={styles.container}>
-                <View display={display_button ? 'flex' : 'none'}>
+                <View>
                     <TextInput
                         onChangeText={(value) => this.onChangeText('full_name', value)}
                         style={styles.input}
                         placeholder="İsim Soyisim"
                         placeholderTextColor={'#434241'}
                     />
-
                     <TextInput
                         onChangeText={(value) => this.onChangeText('cell_phone', value)}
                         style={styles.input}
                         placeholder="Telefon numaranızı giriniz"
                         placeholderTextColor={'#434241'}
+                        keyboardType={'numeric'}
                     />
+                    <Button title={'Devam et'} onPress={() => this.SignUp()} buttonStyle={styles.continueButtonStyle}/>
                 </View>
 
-                <Button title={'KAYIT OL'} onPress={() => this.SignUp()}/>
+                <View style={styles.bottomContainer}>
+                    <View style={styles.textContainerStyle}>
+                        <TouchableOpacity style={{flexDirection: 'row'}}>
+                            <Text style={styles.textStyle}>Devam ederseniz </Text>
+                            <Text style={styles.textStyle2}>Kullanım koşulları</Text>
+                        </TouchableOpacity>
+                        <Text style={styles.textStyle}> 'nı kabul etmiş olursunuz.</Text>
+                    </View>
+                </View>
+
             </View>
         );
     }
@@ -80,18 +93,47 @@ const styles = StyleSheet.create({
     container: {
         flex: 1,
         flexDirection: 'column',
-        justifyContent: 'center',
+        justifyContent: 'space-between',
         alignItems: 'center',
+        marginTop: Dimensions.get('window').width * 0.35,
+        marginHorizontal: Dimensions.get('window').width * 0.2,
     },
     input: {
-        textAlign: 'center',
-        height: 40,
+        textAlign: 'left',
+        padding: 5,
+        height: 50,
+        fontSize: 15,
+        fontWeight: '500',
         color: '#000',
+        backgroundColor: '#F2F3F4',
         width: Dimensions.get('window').width * 0.96,
-        borderBottomWidth: 0.5,
-        borderWidth: 1,
-        borderBottomColor: '#474646',
-        margin: 10,
+        borderWidth: 1.5,
+        borderColor: '#D7DBDD',
+        marginVertical: 10,
+    },
+    textStyle2: {
+        color: '#0497EC',
+        fontSize: 16,
+    },
+    textStyle: {
+        color: '#1f2020',
+    },
+    textContainerStyle: {
+        flexDirection: 'column',
+        justifyContent: 'flex-start',
+        alignItems: 'center',
+    },
+    bottomContainer: {
+        marginVertical: 30,
+        justifyContent: 'center',
+        width: Dimensions.get('window').width * 0.96,
+    },
+    continueButtonStyle: {
+        width: Dimensions.get('window').width * 0.96,
+        height: 50,
+        borderRadius: 25,
+        backgroundColor: '#0497EC',
+        marginVertical: 20,
     },
 });
 
